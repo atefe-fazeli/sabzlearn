d032f34b6aa748aabe404b69efdc1818ee967ff4
atefe fazeli <atefefazeli1996@outlook.com>
2024-09-16 19:36:41 +0330

    landing pae completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
+import { useRoutes } from 'react-router-dom';
 import './App.css';
-
+import routes from './routes';
 function App() {
+  let router=useRoutes(routes)
   return (
     <div >
-
+ { router }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import './styles/reset.css'
+import './styles/fonts.css'
+import './styles/variables.css'
+import './styles/helpers.css'
+import './styles/defaults.css'
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
