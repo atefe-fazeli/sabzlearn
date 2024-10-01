780427a57f48bc2af9e9d532607ac730f09756c7
atefe fazeli <atefefazeli1996@outlook.com>
2024-10-01 14:25:19 +0330

    aboutus components completed

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../../shared/Header/Header";
 import "./Index.css";
 import LastCourses from "../../shared/LastCourses/LastCourses";
+import AboutUs from "../../shared/AboutUs/AboutUs";
 
 export default function Index() {
   return (
     <>
       <Header />
       <LastCourses />
+      <AboutUs/>
     </>
   );
 }
