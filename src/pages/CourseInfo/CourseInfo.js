470b8d2ae6737ccbc8f4e5414748cbc2604be9dc
atefe fazeli <atefefazeli1996@outlook.com>
2024-10-01 20:57:28 +0330

    course detail box created

--- src/pages/CourseInfo/CourseInfo.js
+++ src/pages/CourseInfo/CourseInfo.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 import Topbar from "./../../shared/Topbar/Topbar";
 import Navbar from "./../../shared/Navbar/Navbar";
 import Footer from "./../../shared/Footer/Footer";
 
-import './CourseInfo.css'
-import Breadcrumb from '../../shared/Breadcrumb/Breadcrumb';
+import "./CourseInfo.css";
+import Breadcrumb from "../../shared/Breadcrumb/Breadcrumb";
+import CourseDetailBox from "../../shared/CourseDetailBox/CourseDetailBox";
 
 export default function CourseInfo() {
   return (
@@ -14,13 +15,95 @@ export default function CourseInfo() {
 
       <Breadcrumb
         links={[
-          {id: 1, title: "خانه", to: ''},
-          {id: 2, title: "آموزش برنامه نویسی فرانت‌اند", to: 'category-info/frontend'},
-          {id: 3, title: "دوره متخصص جاوا اسکریپت", to: 'course-info/js-expert'},
+          { id: 1, title: "خانه", to: "" },
+          {
+            id: 2,
+            title: "آموزش برنامه نویسی فرانت‌اند",
+            to: "category-info/frontend",
+          },
+          {
+            id: 3,
+            title: "دوره متخصص جاوا اسکریپت",
+            to: "course-info/js-expert",
+          },
         ]}
       />
 
+      <section class="course-info">
+        <div class="container">
+          <div class="row">
+            <div class="col-6">
+              <a href="#" class="course-info__link">
+                آموزش برنامه نویسی فرانت اند
+              </a>
+              <h1 class="course-info__title">
+                آموزش 20 کتابخانه جاوااسکریپت برای بازار کار
+              </h1>
+              <p class="course-info__text">
+                امروزه کتابخانه‌ها کد نویسی را خیلی آسان و لذت بخش تر کرده اند.
+                به قدری که حتی امروزه هیچ شرکت برنامه نویسی پروژه های خود را با
+                Vanilla Js پیاده سازی نمی کند و همیشه از کتابخانه ها و فریمورک
+                های موجود استفاده می کند. پس شما هم اگه میخواید یک برنامه نویس
+                عالی فرانت اند باشید، باید کتابخانه های کاربردی که در بازار کار
+                استفاده می شوند را به خوبی بلد باشید
+              </p>
+              <div class="course-info__social-media">
+                <a href="#" class="course-info__social-media-item">
+                  <i class="fab fa-telegram-plane course-info__icon"></i>
+                </a>
+                <a href="#" class="course-info__social-media-item">
+                  <i class="fab fa-twitter course-info__icon"></i>
+                </a>
+                <a href="#" class="course-info__social-media-item">
+                  <i class="fab fa-facebook-f course-info__icon"></i>
+                </a>
+              </div>
+            </div>
+
+            <div class="col-6">
+              <video
+                src=""
+                poster="/images/courses/js_project.png"
+                class="course-info__video"
+                controls
+              ></video>
+            </div>
+          </div>
+        </div>
+      </section>
+      <main class="main">
+        <div class="container">
+          <div class="row">
+            <div class="col-8">
+              <div class="course">
+                <div class="course-boxes">
+                  <div class="row">
+                    <CourseDetailBox 
+                      icon='graduation-cap'
+                      title='وضعیت دوره:'
+                      text="به اتمام رسیده"
+                    />
+                    <CourseDetailBox
+                      icon="clock"
+                      title=" مدت زمان دوره:"
+                      text="19 ساعت"
+                    />
+                    <CourseDetailBox
+                      icon="calendar-alt"
+                      title="آخرین بروزرسانی:"
+                      text="1401/03/02"
+                    />
+                    <CourseDetailBox />
+                    <CourseDetailBox />
+                    <CourseDetailBox />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
       <Footer />
     </>
-  )
+  );
 }
