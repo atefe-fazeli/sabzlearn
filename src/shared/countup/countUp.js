bc94b684791effb3c2eb8e41d8ab3988b965a421
atefe fazeli <atefefazeli1996@outlook.com>
2024-10-01 15:37:09 +0330

    countup effect for landing page

---
+++ src/shared/countup/countUp.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+export default function CountUp({ endPoint }) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1);
+    if (count == endPoint) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [count]);
+  return <p>{count}</p>;
+}
