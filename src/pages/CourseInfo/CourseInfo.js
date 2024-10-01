965bcfd61d4bff3f493ec02482479f5deb9b9ce4
atefe fazeli <atefefazeli1996@outlook.com>
2024-10-01 20:36:11 +0330

    breadcrumb component created

--- src/pages/CourseInfo/CourseInfo.js
+++ src/pages/CourseInfo/CourseInfo.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Topbar from "./../../shared/Topbar/Topbar";
+import Navbar from "./../../shared/Navbar/Navbar";
+import Footer from "./../../shared/Footer/Footer";
 
 import './CourseInfo.css'
+import Breadcrumb from '../../shared/Breadcrumb/Breadcrumb';
 
 export default function CourseInfo() {
   return (
-    <div>CourseInfo</div>
+    <>
+      <Topbar />
+      <Navbar />
+
+      <Breadcrumb
+        links={[
+          {id: 1, title: "خانه", to: ''},
+          {id: 2, title: "آموزش برنامه نویسی فرانت‌اند", to: 'category-info/frontend'},
+          {id: 3, title: "دوره متخصص جاوا اسکریپت", to: 'course-info/js-expert'},
+        ]}
+      />
+
+      <Footer />
+    </>
   )
 }
